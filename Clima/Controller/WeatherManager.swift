f45527ef597789b09ba44b1875ca1e18d5643c3b
Nicole Liu <nicoleliu_@outlook.com>
2020-05-26 17:13:13 -0400

    URL session and networking

--- Clima/Controller/WeatherManager.swift
+++ Clima/Controller/WeatherManager.swift
@@ -10,8 +10,41 @@ import Foundation
 
 struct WeatherManager {
     let weatherURL = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=62d67335b71f0077852794fb1ad2224a#"
-
-func fetchWeather(cityName: String){
-    let urlString = "\(weatherURL)&q=\(cityName)"
-}
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    func performRequest (urlString: String){
+        // 1. Create a URL
+        
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URL session
+            
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            
+           let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // 4. Start the task
+            
+            task.resume()
+    
+            
+        }
+    }
+    
+    func handle (data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
 }
--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
   
@@ -42,6 +44,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
         // Use searchTextField.text to get weather for city
         searchTextField.text = ""
     }
